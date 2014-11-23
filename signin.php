2eaba875fceaf6fdf8892fb9cb6b96cea46f2fe2
prontopronto38 <pronto.pronto38@gmail.com>
2014-11-23 18:18:29 +0200

    Update signin.php

--- signin.php
+++ signin.php
@@ -1,3 +1,21 @@
+<html>
+  <body>
+    <?php
+	// [START formprocessing]
+    if (array_key_exists('content', $_POST)) {
+      echo "You wrote:<pre>\n";
+      echo htmlspecialchars($_POST['content']);
+      echo "\n</pre>";
+    }
+	// [END formprocessing]
+    ?>
+    <form action="/sign" method="post">
+      <div><textarea name="content" rows="3" cols="60"></textarea></div>
+      <div><input type="submit" value="Sign Guestbook"></div>
+    </form>
+  </body>
+</html>
+
 <?php
 
 use google\appengine\api\users\User;
